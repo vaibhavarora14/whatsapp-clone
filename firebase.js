72bf4e1974615908cf6090b0ce3eec9e7585b68a
Vaibhav <varora@catalystone.com>
2020-09-06 17:41:57 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApiJSf34kamnEOpDUDtp5AySnDcJDC72Y",
+    authDomain: "whatspp-clone-6c16b.firebaseapp.com",
+    databaseURL: "https://whatspp-clone-6c16b.firebaseio.com",
+    projectId: "whatspp-clone-6c16b",
+    storageBucket: "whatspp-clone-6c16b.appspot.com",
+    messagingSenderId: "916862460097",
+    appId: "1:916862460097:web:4555f62d58be9b02aaf440",
+    measurementId: "G-7NCM016ESD"
+};
